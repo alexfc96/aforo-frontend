ce3ee92b3997a9896fcde1479aa32b9d10de5651
Alex <alex.fernandez.canovas@gmail.com>
2020-06-03 17:14:05 +0200

    added the calendar for the home pagegit add .git add .

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,58 @@
 import React, { Component } from "react";
+import DayPicker from 'react-day-picker';
+
+import 'react-day-picker/lib/style.css';
+import apiBookings from "../services/apiBookings";
+
 
 import { withAuth } from "../context/authContext";
 
 class Home extends Component {
+
+  state = {
+    bookings: undefined,
+	};
+
+  getBookings(){
+    apiBookings
+    .bookings()
+    .then(({ data:bookings }) => {
+      this.setState({
+        bookings
+      });
+    })
+    .catch((error) => {
+      console.log(error)
+      this.setState({
+        bookings: false,
+      });
+    });
+  }
+
+  componentDidMount(){
+    this.getBookings()
+  }
+
   render() {
     const { onLogout, user } = this.props;
     // console.log(user)
+    const { bookings } = this.state;
+    if(bookings){
+      console.log(bookings[0].day)
+    }
+    // var captureDay = new Date();
+
     return (
       <div>
-        <h1>Protected</h1>
-        <p>Welcome {user.name}</p>
-        <button onClick={onLogout}>Logout</button>
+        <h1 style={{textAlign:"left", marginLeft:"5%"}}>Welcome {user.name}</h1><br/>
+
+        <h3>Your next bookings:</h3>
+        {bookings && 
+          <DayPicker selectedDays={bookings.map((booking)=>{
+            return new Date(booking.day)
+          }
+          )} />
+        }
         
       </div>
     );
