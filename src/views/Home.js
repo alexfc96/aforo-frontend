9fab7e5669ab002181f272d9e13b1d4e06035924
Alex <alex.fernandez.canovas@gmail.com>
2020-06-04 13:04:27 +0200

    now appears the favoritees of the establishment in the hoome screen

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import DayPicker from 'react-day-picker';
+import { Link } from "react-router-dom";
+
 //http://react-day-picker.js.org/examples/selected
 
 import 'react-day-picker/lib/style.css';
 import apiBookings from "../services/apiBookings";
-
+import apiClient from "../services/apiClient";
 
 import { withAuth } from "../context/authContext";
 
@@ -12,6 +14,7 @@ class Home extends Component {
 
   state = {
     bookings: undefined,
+    fullUser: undefined
 	};
 
   getBookings(){
@@ -30,26 +33,56 @@ class Home extends Component {
     });
   }
 
+  getUser(){
+    const { user } = this.props;
+    apiClient
+    .getUser(user._id)
+    .then(({ data:fullUser }) => {
+      if(fullUser.favoriteEstablishments.length>0){
+        this.setState({
+          fullUser
+        })
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }
+
   componentDidMount(){
     this.getBookings()
+    this.getUser()
   }
 
   render() {
     const { user } = this.props;
-    const { bookings } = this.state;
-    console.log(user)
+    const { bookings, fullUser } = this.state;
+    console.log(fullUser)
     return (
       <div>
         <h1 style={{textAlign:"left", marginLeft:"5%"}}>Welcome {user.name}</h1><br/>
 
-        <h2>Your next bookings:</h2>
+        <h3 style={{margin: "-20px 0 10px 0"}}>Your next bookings:</h3>
         {bookings && 
           <DayPicker selectedDays={bookings.map((booking)=>{
             return new Date(booking.day)
           }
           )} />
         }
-        
+        {fullUser && 
+          <div>
+            <h3 style={{margin: "0 0 10px 0"}}>Your favorites establishments:</h3>
+            <ul>
+              {fullUser.favoriteEstablishments.map((establishment) =>{
+                return <li style={{display: "flex", marginBottom:"2%"}}><Link to={`/establishment/${establishment._id}`}>·{establishment.name}</Link></li>
+              })
+
+              }
+
+            </ul>
+
+          </div>
+        }
       </div>
     );
   }
